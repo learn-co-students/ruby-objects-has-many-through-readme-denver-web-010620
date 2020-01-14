ed8bb066ff1b22ea8f8cb0eee1db0480bd12fd26
Doug Johnson <douglas.j.johnson@gmail.com>
2020-01-14 14:15:51 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
 class Customer
-  
+    attr_reader :name, :age
+
+    def initialize name, age
+        @name = name
+        @age = age
+        @@all << self
+    end
+    
+    def new_meal waiter, total, tip
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.map {|meal| meal.waiter}.uniq
+    end
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
 end
